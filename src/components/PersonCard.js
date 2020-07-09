a0940197e99774f0890b42c1814fbc27315063e1
dathanwong <dwong5@villanova.edu>
2020-07-09 14:38:38 -0700

    Added Putting it together assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class PersonCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            age: props.age
+        };
+    }
+
+    addBirthday = () =>{
+        this.setState({
+            age: this.state.age+1
+        }) 
+    }
+
     render(){
         return (
             <div class="container justify-content-center">
@@ -8,11 +21,17 @@ class PersonCard extends React.Component{
                     <h1>{this.props.lastName}, {this.props.firstName}</h1>
                 </div>
                 <div class="row">
-                    Age: {this.props.age}
+                    Age: {this.state.age}
                 </div>
                 <div class="row">
                     Hair Color: {this.props.hairColor}
                 </div>
+                <div class="row">
+                    <div class="col">
+                        <button onClick={this.addBirthday} class="btn btn-secondary">Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+                    </div>
+                </div>
+                {this.props.children}
             </div>
         );
     }
